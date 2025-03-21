b4f794a63228dd8f3117ebe6b141f64679e97474
Josesacrab <josesacrab2003@gmail.com>
2025-03-21 17:16:11 -0600

    Add files via upload

---
+++ archivo secreto/anim.js
@@ -0,0 +1,72 @@
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Datos de las letras
+var lyricsData = [
+  { text: "At the time", time: 15 },
+  { text: "The whisper of birds", time: 18 },
+  { text: "Lonely before the sun cried", time: 27 },
+  { text: "Fell from the sky", time: 32 },
+  { text: "Like water drops", time: 33 },
+  { text: "Where I'm now? I don't know why", time: 41 },
+  { text: "Nice butterflies in my hands", time: 47 },
+  { text: "Too much light for twilight", time: 54 },
+  { text: "In the mood for the flowers love", time: 59 },
+  { text: "That vision", time: 67 },
+  { text: "Really strong, blew my mind", time: 72 },
+  { text: "Silence Let me see what it was", time: 78 },
+  { text: "I only want to live in clouds", time: 83 },
+  { text: "Where I'm now? I don't know why", time: 91 },
+  { text: "Nice butterflies in my hands", time: 97 },
+  { text: "Too much light for twilight", time: 104 },
+  { text: "In the mood for the flowers love", time: 108 },
+  { text: "At the time", time: 144 },
+  { text: "The whisper of birds", time: 148 },
+  { text: "Lonely before the sun cried", time: 153 },
+  { text: "Fell from the sky", time: 158 },
+  { text: "Like water drops", time: 164 },
+  { text: "Where I'm now? I don't know why", time: 169 },
+  { text: "Nice butterflies in my hands", time: 176 },
+  { text: "Too much light for twilight", time: 183 },
+  { text: "In the mood for the flowers", time: 188 },
+  { text: "Love.", time: 140 },
+];
+
+// Función para actualizar las letras en tiempo real
+function updateLyrics() {
+  var time = audio.currentTime;
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    var fadeInDuration = 0.5; // Ajustar duración del fade-in
+    var timeElapsed = time - currentLine.time;
+    var opacity = Math.min(1, timeElapsed / fadeInDuration);
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+
+  // Llamar de nuevo a la función en el próximo frame de animación
+  requestAnimationFrame(updateLyrics);
+}
+
+// Iniciar la actualización de las letras cuando comience la canción
+audio.addEventListener("play", function () {
+  requestAnimationFrame(updateLyrics);
+});
+
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation = "fadeOut 3s ease-in-out forwards";
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000);
+}
+
+// Llama a la función después de 216 segundos
+setTimeout(ocultarTitulo, 216000);
